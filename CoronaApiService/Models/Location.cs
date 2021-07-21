681956d7dd72ee0eeafd38776f52811f4305c7c0
ron93 <ronmkare1@gmail.com>
2021-07-21 13:57:56 +0000

    location data model

---
+++ CoronaApiService/Models/Location.cs
@@ -0,0 +1,26 @@
+using System;
+using Newtonsoft.Json;
+
+namespace CoronaApiService.Models
+{
+    public class Location
+    {
+        [JsonProperty("id")]
+        public int Id { get; set;}
+        [JsonProperty("country")]
+        public string Country { get; set; }
+        [JsonProperty("country_code")]
+        public string CountryCode { get; set; }
+        [JsonProperty("country_population")]
+        public int CountryPopulation { get; set; }
+        [JsonProperty("province")]
+        public string Province { get; set; }
+        [JsonProperty("last_updated")]
+        public DateTime LastUpdated { get; set; }
+        [JsonProperty("coordinates")]
+        public Coordinates Coordinates { get; set; }
+        [JsonProperty("latest")]
+        public Latest Latest { get; set; }
+    }
+
+}
